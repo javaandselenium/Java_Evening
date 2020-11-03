1340a6a5b69fb41dd348092a77e71bba21840376
QSP <QSP@DESKTOP-EVK9GMC>
2020-11-03 20:11:00 +0530

    Tokens

---
+++ src/basics/Test0.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class Test0 {
+
+	public static void main(String[] args) {
+		System.out.println(true);
+		System.out.println("Hello JAVA");
+		System.out.println('A');
+		System.out.println(1234);
+		System.out.println(3.89);
+		
+
+	}
+
+}
---
+++ src/basics/Test1.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	System.out.println(20+20);
+	System.out.println("20+20");
+	System.out.println("The value is "+20+20);
+	System.out.println(20+20+"The value is");
+	System.out.println("The value is "+(20+20));
+		
+	}
+
+}
