d285d3e75531137bf3ca75b7796b9583f21dc148
QSP <QSP@DESKTOP-EVK9GMC>
2020-11-24 19:41:38 +0530

    ArraysExamples

---
+++ src/arrays/Car.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class Car {
+
+	public static void main(String[] args) {
+	Car[] c=new Car[3];
+	for(int i=0;i<=2;i++)
+	System.out.println(c[i]);
+	System.out.println(c);
+
+	}
+
+}
---
+++ src/arrays/Test9.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class Test9 {
+
+	public static void main(String[] args) {
+	char a[]=new char[3];
+	a[0]='A';
+	a[1]='B';
+	a[1]='C';
+	for(int i=0;i<3;i++) {
+		System.out.println(a[i]);
+	}
+
+	}
+
+}
